3a5a5c198751a40f5c8905ff3ca77fafd9e9ea53
Marco Siebecke <gentoomaniac@gmail.com>
2021-01-12 10:58:41 +0100

    Name consistency

--- locales/enUS.lua
+++ locales/enUS.lua
@@ -1,6 +1,6 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("VaultMinimapIcon", "enUS", true, debug)
+local L = LibStub("AceLocale-3.0"):NewLocale("VaultMinimapButton", "enUS", true, debug)
 if L then
-    L["ADDONNAME"] = "VaultMinimapIcon"
+    L["ADDONNAME"] = "VaultMinimapButton"
     L["REGIONERROR"] = "Couldn't find region!"
     L["NO_REWARDS"] = "You have no rewards."
     L["REWARDS_AVAILABLE"] = "Pick up your rewards! :)"
