82488ab240343e44adbef91aeb58963f9264984a
Marco Siebecke <gentoomaniac@gmail.com>
2021-01-14 03:07:11 +0100

    make PvP one line and use symbols for finishing

--- core.lua
+++ core.lua
@@ -37,21 +37,43 @@ local ldb = LibStub("LibDataBroker-1.1"):NewDataObject("VaultMinimapButton", {
         tt:AddDoubleLine(L["RAID"])
         tt:AddDoubleLine(" ")
 
-        local sampleItem = C_WeeklyRewards.GetExampleRewardItemHyperlinks(activities[Enum.WeeklyRewardChestThresholdType.RankedPvP][1].id)
-        local iLvl = GetDetailedItemLevelInfo(sampleItem);
-        if iLvl then
-            tt:AddDoubleLine(string.format(L["RANKEDPVP_LOOT"], iLvl))
-        else
-            tt:AddDoubleLine(L["RANKEDPVP"])
-        end
-        for _, activity in pairs(activities[Enum.WeeklyRewardChestThresholdType.RankedPvP]) do
-            tt:AddLine(GetRatedPvPActivityString(activity))
-        end
+        pvpHeader, pvpText = GetPvPText(activities[Enum.WeeklyRewardChestThresholdType.RankedPvP])
+        tt:AddDoubleLine(pvpHeader)
+        tt:AddLine(pvpText)
         tt:AddDoubleLine(" ")
 
         --GetWeeksMythicPlusRuns()
     end
 })
+
+function GetPvPText(activities)
+    local header = ""
+    local text = ""
+
+    if activities[1].progress >= activities[1].threshold then
+        local sampleItem = C_WeeklyRewards.GetExampleRewardItemHyperlinks(activities[1].id)
+        local iLvl = GetDetailedItemLevelInfo(sampleItem);
+        header = string.format(L["RANKEDPVP_LOOT"], iLvl)
+        text = "\124T" .. READY_CHECK_READY_TEXTURE .. ":0|t"
+    else
+        header = L["RANKEDPVP"]
+        text = "|cFFFF9900" .. tostring(activities[1].threshold - activities[1].progress) .. "|r"
+    end
+
+    if activities[2].progress >= activities[2].threshold then
+        text = text .. "\124T" .. READY_CHECK_READY_TEXTURE .. ":0|t"
+    else
+        text = text .. " / |cFFFF9900" .. tostring(activities[2].threshold - activities[2].progress) .. "|r"
+    end
+
+    if activities[3].progress >= activities[3].threshold then
+        text = text .. "\124T" .. READY_CHECK_READY_TEXTURE .. ":0|t"
+    else
+        text = text .. " / |cFFFF9900" .. tostring(activities[3].threshold-activities[3].progress) .. "|r"
+    end
+
+    return header, text
+end
 
 local icon = LibStub("LibDBIcon-1.0")
 
@@ -116,15 +138,6 @@ end
 function GetIncompleteMythicPlusActivityILvl(rewardLevel)
     mythicPlusRuns = C_MythicPlus.GetRunHistory(false, true)
     print(dump(mythicPlusRuns))
-
-end
-
-function GetRatedPvPActivityString(activity)
-    if activity.progress >= activity.threshold then
-        return string.format(L["RANKEDPVP_REACHED"], iLvl)
-    else
-        return string.format(L["RANKEDPVP_NOT_REACHED"], activity.threshold - activity.progress)
-    end
 
 end
 
